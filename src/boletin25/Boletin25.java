e51f1f5cc21d71bf4775cc1024342f6b6b8be89b
slorenzorodriguez <slorenzorodriguez@nautilus22.danielcastelao.org>
2019-06-03 10:44:23 +0200

    B25

---
+++ src/boletin25/Boletin25.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package boletin25;
+
+/**
+ *
+ * @author slorenzorodriguez
+ */
+public class Boletin25 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+     Ventana obx = new Ventana();
+        obx.setVisible(true);
+        
+        
+        
+    }
+    
+}
